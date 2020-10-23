596de136bf5bebaed8825abc6f3b1680e7cb63d0
RealXiaoLin <yukixechan@gmail.com>
2020-10-23 09:38:07 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  has_one_attached :image
+  # :imageと設定することで、フォームから送られるパラメーターのキーに
 
   validates :content, presence: true
 end
